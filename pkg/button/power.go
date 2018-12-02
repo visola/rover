05f2300f3f850ed708caa79ab79b6c774f0e8da2
Vinicius Isola <viniciusisola@gmail.com>
2018-12-02 18:03:22 -0500

    Require > 5s press to shutdown

--- pkg/button/power.go
+++ pkg/button/power.go
@@ -6,39 +6,69 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"time"
 
 	"github.com/visola/rover/pkg/finalizer"
+	"github.com/visola/rover/pkg/light"
 	"gobot.io/x/gobot/drivers/gpio"
 	"gobot.io/x/gobot/platforms/raspi"
 )
 
 var powerOff *gpio.ButtonDriver
 
+var powerOffDown = false
+var powerOffPressedAt time.Time
+
 // InitializePowerOff prepares the power off button
 func InitializePowerOff(adaptor *raspi.Adaptor) {
 	powerOff = gpio.NewButtonDriver(adaptor, "13")
-	powerOff.Start()
 	finalizer.AddFinalizer(func() {
 		powerOff.Halt()
 	})
 
-	powerOff.On(gpio.ButtonRelease, func(_ interface{}) {
-		fmt.Println("Powering off...")
-
-		cmd := exec.Command("sudo", "shutdown", "-h", "now")
-		var stdOut bytes.Buffer
-		var stdErr bytes.Buffer
-		cmd.Stdout = &stdOut
-		cmd.Stderr = &stdErr
-
-		err := cmd.Run()
-		if err != nil {
-			log.Fatal(err)
+	go func() {
+		for {
+			if powerOffDown {
+				diff := time.Now().Sub(powerOffPressedAt).Seconds()
+				if diff > 5 {
+					light.FrontLightOff()
+					shutdown()
+				}
+			}
+			time.Sleep(1 * time.Second)
 		}
+	}()
 
-		fmt.Printf("Standard out:\n%q\n", stdOut.String())
-		fmt.Printf("Standard err:\n%q\n", stdErr.String())
-
-		os.Exit(0)
+	powerOff.On(gpio.ButtonPush, func(_ interface{}) {
+		powerOffDown = false
+		light.FrontLightOff()
 	})
+
+	powerOff.On(gpio.ButtonRelease, func(_ interface{}) {
+		powerOffPressedAt = time.Now()
+		powerOffDown = true
+		light.FrontLightOn()
+	})
+
+	powerOff.Start()
+}
+
+func shutdown() {
+	fmt.Println("Powering off...")
+
+	cmd := exec.Command("sudo", "shutdown", "-h", "now")
+	var stdOut bytes.Buffer
+	var stdErr bytes.Buffer
+	cmd.Stdout = &stdOut
+	cmd.Stderr = &stdErr
+
+	err := cmd.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("Standard out:\n%q\n", stdOut.String())
+	fmt.Printf("Standard err:\n%q\n", stdErr.String())
+
+	os.Exit(0)
 }
